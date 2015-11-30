37fd89d405410ab3f8d091122245b71070a17069
Arif Arman <arif.arman046@gmail.com>
2015-11-30 18:03:44 +0600

    delaunay triangulation added, has bugs

---
+++ src/Point.java
@@ -0,0 +1,18 @@
+
+public class Point {
+	double x;
+	double y;
+	
+	public Point(double x, double y) {
+		// TODO Auto-generated constructor stub
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		String s = x + " " + y;
+		return s;
+	}
+}
